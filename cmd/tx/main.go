8ff088af620176974f3361cdbec0e597ee1b5695
Max Kuznetsov <maks.kuznetsov@gmail.com>
2026-03-13 09:56:55 +0000

    releases

--- cmd/tx/main.go
+++ cmd/tx/main.go
@@ -1,16 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"runtime/debug"
+	"strings"
 
 	flags "github.com/jessevdk/go-flags"
 	"github.com/texops/tx/internal/cli"
 )
 
+var version string
+
+func resolveVersion() string {
+	if version != "" {
+		return version
+	}
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return "dev"
+	}
+	if v := info.Main.Version; v != "" && v != "(devel)" {
+		return strings.TrimPrefix(v, "v")
+	}
+	return "dev"
+}
+
 func main() {
 	var opts cli.Options
 	parser := flags.NewParser(&opts, flags.Default)
 	parser.Name = "tx"
+
+	if len(os.Args) == 2 && (os.Args[1] == "--version" || os.Args[1] == "version") {
+		fmt.Println("tx " + resolveVersion())
+		os.Exit(0)
+	}
 
 	ui := cli.NewUI(os.Stdout)
 	opts.Login.UI = ui
